2fc04eda0571dc59b59ccda366e5b938092c117b
radair86 <radair86@gmail.com>
2023-08-15 12:42:45 -0500

    ai.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,47 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
+#Install necessary dependencies
 import streamlit as st
+import pprint
+import google.generativeai as palm
+from langchain.chains import ConversationChain
+from langchain.memory import ConversationBufferMemory
+from langchain.llms import VertexAI
+from langchain import PromptTemplate, LLMChain
+from gtts import gTTS
+from io import BytesIO
+import tempfile
 
-"""
-# Welcome to Streamlit!
+#Mention your api key here provided to you by https://makersuite.google.com/app
+palm.configure(api_key=st.secrets['PALM-API-KEY'])
+models = [m for m in palm.list_models() if 'generateText' in m.supported_generation_methods]
+model = models[0].name
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+#Title of application
+st.title("A chatbot with the capabilities of PalmAI 🤖")
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+#Setting up the prompt area of app
+prompt = st.text_input("Enter your question here..")
+if prompt:
+    completion = palm.generate_text(
+        model=model,
+        prompt=prompt,
+        temperature=0.7,
+        # The maximum length of the response
+        max_output_tokens=800
+)
+    st.write(completion.result)
+    #Using google-text-to-speech library
+    tts = gTTS(completion.result, lang='en')
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+# Save audio to temporary file
+    with tempfile.NamedTemporaryFile(delete=False) as fp:
+        tts.save(fp.name)
+        audio_path = fp.name
 
+# Read the audio file
+    with open(audio_path, 'rb') as f:
+        audio_bytes = f.read()
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+# Play the audio
+    st.audio(audio_bytes, format='audio/mp3')
+else:
+    st.write("type something and hit enter...")
